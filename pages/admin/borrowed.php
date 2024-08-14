ced191e9826e0f1f55949fcd69c96e3339894814
Vidula Deneth <viduladenethsalwathura@gmail.com>
2024-08-15 05:14:04 +0530

    borrowed search result code optimized

--- pages/admin/borrowed.php
+++ pages/admin/borrowed.php
@@ -5,127 +5,77 @@ include('../../includes/functions/check_admin.php');
 $error = null;
 $html = null;
 
+// Function to process the results of the SQL query
+function processResults($resultBookID) {
+    $html = '';
+    if (mysqli_num_rows($resultBookID) > 0) {
+        while ($row = mysqli_fetch_assoc($resultBookID)) {
+            $checkDue = $row['return_date'] == null ? '<button>Receive</button>' : $row['return_date'];
+            $html .= '<form action="borrowed.php" method="post">
+                        <tr>
+                            <td>' . $row['book_id'] . '</td>
+                            <td>' . $row['name'] . '</td>
+                            <td>' . $row['username'] . '</td>
+                            <td>' . $row['mobile_no'] . '</td>
+                            <td>' . $row['borrow_date'] . '</td>
+                            <td>' . $checkDue . '</td>
+                            <td>Yes</td>
+                        </tr>
+                      </form>';
+        }
+    } else {
+        $html .= '<tr><td colspan="7">No results found.</td></tr>';
+    }
+    return $html;
+}
+
 // Check if the search form has been submitted
 if (isset($_POST['search-submit'])) {
     // Get the search details from the form
     $search_by = $_POST['search-by'];
     $search_value = $_POST['search'];
 
-    // Check if the search is by book ID 
-    if ($search_by == 'book-id') {
-        // Check if the search value is a number
-        if (is_numeric($search_value)) {
-            // SQL query to get borrowed books by book ID
-            $sqlBookID = "SELECT bb.* , b.name , u.username , u.mobile_no FROM borrowed_books bb
-                            JOIN books b ON bb.book_id = b.books_id 
-                            JOIN users u ON bb.user_id = u.user_id
+    // Build the SQL query based on the search type
+    switch ($search_by) {
+        case 'book-id':
+            if (is_numeric($search_value)) {
+                $sql = "SELECT bb.*, b.name, u.username, u.mobile_no 
+                        FROM borrowed_books bb
+                        JOIN books b ON bb.book_id = b.books_id 
+                        JOIN users u ON bb.user_id = u.user_id
                         WHERE bb.book_id = $search_value";
-
-            // Execute the query
-            $resultBookID = mysqli_query($conn, $sqlBookID);
-            if ($resultBookID) {
-                //check if there any result
-                if (mysqli_num_rows($resultBookID) > 0) {
-                    // Fetch and display the results
-                    while ($row = mysqli_fetch_assoc($resultBookID)) {
-                        $checkDue = $row['return_date'] == null ? '<button>Receive</button>' : $row['return_date'];
-                        $html .= '<form action="borrowed.php" method="post">
-                                    <tr>
-                                        <td>' . $row['book_id'] . '</td>
-                                        <td>' . $row['name'] . '</td>
-                                        <td>' . $row['username'] . '</td>
-                                        <td>' . $row['mobile_no'] . '</td>
-                                        <td>' . $row['borrow_date'] . '</td>
-                                        <td>' . $checkDue . '</td>
-                                        <td>Yes</td>
-                                    </tr>
-                                  </form>';
-                    }
-                } else {
-                    // Display a message if no results are found
-                    $html .= '<tr><td colspan="7">No results found.</td></tr>';
-                }
             } else {
-                // Display an error if the query fails
-                $error = '<h3 class="warning">Error executing query!</h3>';
+                $error = '<h3 class="warning">Please enter a valid book ID!</h3>';
             }
-        } else {
-            // Display an error if the book ID is not a number
-            $error = '<h3 class="warning">Please enter a valid book ID!</h3>';
-        }
+            break;
+        
+        case 'user-mobile':
+            $sql = "SELECT bb.*, b.name, u.username, u.mobile_no 
+                    FROM borrowed_books bb
+                    JOIN books b ON bb.book_id = b.books_id 
+                    JOIN users u ON bb.user_id = u.user_id
+                    WHERE u.mobile_no = '$search_value'";
+            break;
 
-    // Check if the search is by user mobile number
-    } else if ($search_by == 'user-mobile') {
-        // SQL query to get borrowed books by user mobile number
-        $sqlBookID = "SELECT bb.*, b.name, u.username, u.mobile_no 
-                      FROM borrowed_books bb
-                      JOIN books b ON bb.book_id = b.books_id 
-                      JOIN users u ON bb.user_id = u.user_id
-                      WHERE u.mobile_no = '$search_value'";
+        case 'user':
+            $sql = "SELECT bb.*, b.name, u.username, u.mobile_no 
+                    FROM borrowed_books bb
+                    JOIN books b ON bb.book_id = b.books_id 
+                    JOIN users u ON bb.user_id = u.user_id
+                    WHERE u.username LIKE '%$search_value%'";
+            break;
 
-        // Execute the query
-        $resultBookID = mysqli_query($conn, $sqlBookID);
+        default:
+            $error = '<h3 class="warning">Invalid search criteria!</h3>';
+            break;
+    }
+
+    // Execute the query if no errors
+    if (!$error && isset($sql)) {
+        $resultBookID = mysqli_query($conn, $sql);
         if ($resultBookID) {
-            // Check if there are any results
-            if (mysqli_num_rows($resultBookID) > 0) {
-                // Fetch and display the results
-                while ($row = mysqli_fetch_assoc($resultBookID)) {
-                    $checkDue = $row['return_date'] == null ? '<button>Receive</button>' : $row['return_date'];
-                    $html .= '<form action="borrowed.php" method="post">
-                                <tr>
-                                    <td>' . $row['book_id'] . '</td>
-                                    <td>' . $row['name'] . '</td>
-                                    <td>' . $row['username'] . '</td>
-                                    <td>' . $row['mobile_no'] . '</td>
-                                    <td>' . $row['borrow_date'] . '</td>
-                                    <td>' . $checkDue . '</td>
-                                    <td>Yes</td>
-                                </tr>
-                              </form>';
-                }
-            } else {
-                // Display a message if no results are found
-                $html .= '<tr><td colspan="7">No results found.</td></tr>';
-            }
+            $html = processResults($resultBookID);
         } else {
-            // Display an error if the query fails
-            $error = '<h3 class="warning">Error executing query!</h3>';
-        }
-    }// Check if the search is by username
-    else if ($search_by == 'user') {
-        // SQL query to get borrowed books by username
-        $sqlBookID = "SELECT bb.*, b.name, u.username, u.mobile_no 
-                      FROM borrowed_books bb
-                      JOIN books b ON bb.book_id = b.books_id 
-                      JOIN users u ON bb.user_id = u.user_id
-                      WHERE u.username LIKE '%$search_value%'";
-
-        // Execute the query
-        $resultBookID = mysqli_query($conn, $sqlBookID);
-        if ($resultBookID) {
-            // Check if there are any results
-            if (mysqli_num_rows($resultBookID) > 0) {
-                // Fetch and display the results
-                while ($row = mysqli_fetch_assoc($resultBookID)) {
-                    $checkDue = $row['return_date'] == null ? '<button>Receive</button>' : $row['return_date'];
-                    $html .= '<form action="borrowed.php" method="post">
-                                <tr>
-                                    <td>' . $row['book_id'] . '</td>
-                                    <td>' . $row['name'] . '</td>
-                                    <td>' . $row['username'] . '</td>
-                                    <td>' . $row['mobile_no'] . '</td>
-                                    <td>' . $row['borrow_date'] . '</td>
-                                    <td>' . $checkDue . '</td>
-                                    <td>Yes</td>
-                                </tr>
-                              </form>';
-                }
-            } else {
-                // Display a message if no results are found
-                $html .= '<tr><td colspan="7">No results found.</td></tr>';
-            }
-        } else {
-            // Display an error if the query fails
             $error = '<h3 class="warning">Error executing query!</h3>';
         }
     }
